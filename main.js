a57be8bceb0d33feb98e4fb9055a6071875a2f36
Mariam Lukava <mariamlukava@gmail.com>
2024-08-28 03:42:30 +0400

    add task2,3,4, and 5

--- main.js
+++ main.js
@@ -13,3 +13,50 @@ for(let i = 1; i<=100; i++){
         console.log(i);
     }
 }
+
+//task 2
+let min = 1;
+let max = 100;
+for(let i = 0; i<100; i++){
+    let randomNumber = Math.floor(Math.random() * (max - min)) +min;
+    console.log(randomNumber);
+}
+
+
+//task 3
+let sum = 0;
+for(let i = 1; i<=1000;i++){
+    if(i%2 === 0){
+        sum+=i;
+    }
+}
+console.log(sum);
+
+
+//task 4
+
+sum = 0;
+
+for(let i =1; i<=1000; i++){
+    if(i%3===0 || i%5===0){
+      sum+=i;
+    }
+}
+console.log(sum);
+
+
+//task 5
+
+sum = 0;
+
+for(let i =1; i<=1000; i++){
+    if(i%3===0 && i%5===0){
+      sum+=i;
+    }
+}
+console.log(sum);
+
+
+
+
+
